357aff8e14ebad745c01ebf964465a18637f7295
Ryan Rundle <ryan.rundle@ymail.com>
2017-06-28 16:49:01 -0700

    final.

--- public/main.js
+++ public/main.js
@@ -1,32 +1,59 @@
 document.addEventListener('click', function(e) {
-  if (e.target.className.indexOf('about') !== -1) {
-    var about = document.querySelector('.about')
-    zenscroll.to(about, 1000)
-  }
-  if (e.target.className.indexOf('projects') !== -1) {
-    var projects = document.querySelector('.projects')
-    zenscroll.to(projects, 1000)
-  }
-  if (e.target.className.indexOf('contact') !== -1) {
-    var contact = document.querySelector('.contact')
-    zenscroll.to(contact, 1000)
+  if (e.target.className.indexOf('when') !== -1) {
+    console.log('clicked!');
+    var date = document.querySelector('.date')
+    zenscroll.to(date, 1000)
   }
 
+  if (e.target.className.indexOf('where') !== -1) {
+    var santaAna = document.querySelector('.santaana')
+    zenscroll.to(santaAna, 1000)
+  }
+
+  if (e.target.className.indexOf('huh') !== -1) {
+    var embassy = document.querySelector('.embassy')
+    zenscroll.to(embassy, 1000)
+  }
+
+  if (e.target.className.indexOf('it') !== -1) {
+    var list = document.querySelector('.list')
+    zenscroll.to(list, 1000)
+  }
+
+  if (e.target.className.indexOf('how') !== -1) {
+    var final = document.querySelector('.final')
+    zenscroll.to(final, 1000)
+  }
+
+  if (e.target.className.indexOf('no') !== -1) {
+    var restart = document.querySelector('.restart')
+    zenscroll.to(restart, 1000)
+  }
+
+  if (e.target.className.indexOf('ok') !== -1) {
+    var header = document.querySelector('.final')
+    zenscroll.to(header, 1000)
+  }
 
   //End of adventure choice
   if (e.target.className.indexOf('yes') !== -1) {
     var contact = document.querySelector('.steps')
-    contact.parent.removeChild(contact)
+    contact.parentNode.removeChild(contact)
+
+    var header = document.querySelector('.header')
+    header.textContent = ''
 
     var yes = document.createElement('div')
     yes.textContent = 'YES!!'
-  }
+    yes.setAttribute('id', 'confirm')
 
-  if (e.target.className.indexOf('no') !== -1) {
-    var contact = document.querySelector('.steps')
-    contact.parent.removeChild(contact)
+    var love = document.createElement('div')
+    love.textContent = "I love you so much!"
+    love.setAttribute('id', 'love')
 
-    var no = document.createElement('div')
-    no.textContent = 'NO!!'
+    var title = document.querySelector('.title')
+    title.appendChild(yes)
+
+    yes.appendChild(love)
   }
 })
