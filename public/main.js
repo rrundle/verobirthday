80f6883583cc0c59cc7d728687c52e0bf437df62
Ryan Rundle <ryan.rundle@ymail.com>
2017-06-28 18:09:48 -0700

    changing things.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,64 @@
-window.document.addEventListener('click' || 'touchstart', function(e) {
+window.document.addEventListener('click', function(e) {
+  if (e.target.className.indexOf('when') !== -1) {
+    console.log('clicked!');
+    var date = document.querySelector('.date')
+    zenscroll.to(date, 1000)
+  }
+
+  if (e.target.className.indexOf('where') !== -1) {
+    var santaAna = document.querySelector('.santaana')
+    zenscroll.to(santaAna, 1000)
+  }
+
+  if (e.target.className.indexOf('huh') !== -1) {
+    var embassy = document.querySelector('.embassy')
+    zenscroll.to(embassy, 1000)
+  }
+
+  if (e.target.className.indexOf('it') !== -1) {
+    var list = document.querySelector('.list')
+    zenscroll.to(list, 1000)
+  }
+
+  if (e.target.className.indexOf('how') !== -1) {
+    var final = document.querySelector('.final')
+    zenscroll.to(final, 1000)
+  }
+
+  if (e.target.className.indexOf('no') !== -1) {
+    var restart = document.querySelector('.restart')
+    zenscroll.to(restart, 1000)
+  }
+
+  if (e.target.className.indexOf('ok') !== -1) {
+    var header = document.querySelector('.final')
+    zenscroll.to(header, 1000)
+  }
+
+  //End of adventure choice
+  if (e.target.className.indexOf('yes') !== -1) {
+    var contact = document.querySelector('.steps')
+    contact.parentNode.removeChild(contact)
+
+    var header = document.querySelector('.header')
+    header.textContent = ''
+
+    var yes = document.createElement('div')
+    yes.textContent = 'YES!!'
+    yes.setAttribute('id', 'confirm')
+
+    var love = document.createElement('div')
+    love.textContent = "I love you so much!"
+    love.setAttribute('id', 'love')
+
+    var title = document.querySelector('.title')
+    title.appendChild(yes)
+
+    yes.appendChild(love)
+  }
+})
+
+window.document.addEventListener('touchstart', function(e) {
   if (e.target.className.indexOf('when') !== -1) {
     console.log('clicked!');
     var date = document.querySelector('.date')
